5bcbb91d3abc06a08c108ded74bcdfa938a2dfe8
RespiceFinem <troelsringboel@gmail.com>
2017-09-20 10:24:07 +0200

    writeFile function commented out

--- nodejs_server/init.js
+++ nodejs_server/init.js
@@ -4,9 +4,9 @@ var serveStatic = require('serve-static');
 connect().use(serveStatic('../website/')).listen(8080, function(){
     console.log('Server running on 8080...');
 });
-var fs = require('fs');
-fs.writeFile("test.txt", jsonData, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-});
+//var fs = require('fs');
+//fs.writeFile("test.txt", jsonData, function(err) {
+//    if(err) {
+//        return console.log(err);
+//    }
+//});
