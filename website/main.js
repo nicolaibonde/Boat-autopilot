1d7113e6c9b75a98b897db9701b3016cc17825b2
neobonde <201404519@post.au.dk>
2017-09-20 10:03:34 +0200

    Added functionallity to add parameters to a profile

--- website/main.js
+++ website/main.js
@@ -1,90 +1,109 @@
 var app = angular.module("myApp", ["ngRoute"]);
 app.config(function($routeProvider) {
-	$routeProvider
-		.when("/", {
-			templateUrl: "home.htm",
-		})
-		.when("/p2p", {
-			templateUrl: "p2p.htm",
-		})
-		.when("/coverage", {
-			templateUrl: "coverage.htm"
-		})
-		.when("/status", {
-			templateUrl: "status.htm"
-		})
-		.when("/editparameters", {
-			templateUrl: "editparameters.htm"
-		})
-		.otherwise({
-			template: "<h1><i class='glyphicon glyphicon-bullhorn'></i> Ups?</h1><p>I can't find that page, sorry</p><p>Maybe call support for me? <i class='glyphicon glyphicon-earphone'></i></p>"
-		});
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.htm",
+        })
+        .when("/p2p", {
+            templateUrl: "p2p.htm",
+        })
+        .when("/coverage", {
+            templateUrl: "coverage.htm"
+        })
+        .when("/status", {
+            templateUrl: "status.htm"
+        })
+        .when("/editparameters", {
+            templateUrl: "editparameters.htm"
+        })
+        .otherwise({
+            template: "<h1><i class='glyphicon glyphicon-bullhorn'></i> Ups?</h1><p>I can't find that page, sorry</p><p>Maybe call support for me? <i class='glyphicon glyphicon-earphone'></i></p>"
+        });
 });
 app.controller('navCtrl', function($scope, $location) {
 
-	$scope.selectedTab = $location.path();
+    $scope.selectedTab = $location.path();
 });
 
 
 
 app.controller('profileCtrl', function($scope, $http) {
-	$http.get("profiles.json").then(function(response) {
+    $http.get("profiles.json").then(function(response) {
         $scope.profiles = response.data.profiles;
-		$scope.selectedProfile = $scope.profiles[0];
+        $scope.selectedProfile = $scope.profiles[0];
     });
 
-	$scope.newProfile = function(){
+    $scope.newProfile = function() {
 
-		$scope.profiles.push({
-			name:"New Profile",
-			parameterNames:[
-				"P","I","D","Tool Width"
-			],
-			parameters:[
-				0,0,0,0
-			],
-			creationTimestamp:Date.now()
-		});
-		$scope.selectedProfile = $scope.profiles[($scope.profiles).length-1];
+        $scope.profiles.push({
+            name: "New Profile",
+            parameterNames: [
+                "P", "I", "D", "Tool Width"
+            ],
+            parameters: [
+                0, 0, 0, 0
+            ],
+            creationTimestamp: Date.now()
+        });
+        $scope.selectedProfile = $scope.profiles[($scope.profiles).length - 1];
 
-		saveProfiles();
-	}
+        saveProfiles();
+    }
 
-	$scope.deleteProfile = function(){
-		if($scope.editingParam != true){
-			$scope.profiles.splice($scope.profiles.indexOf($scope.selectedProfile), 1)
-			$scope.selectedProfile = $scope.profiles[0];
-		}else{
-			//window.alert("Can't delete a profile while editing!");
+    $scope.deleteProfile = function() {
+        if ($scope.editingParam != true) {
+            $scope.profiles.splice($scope.profiles.indexOf($scope.selectedProfile), 1)
+            $scope.selectedProfile = $scope.profiles[0];
+        } else {
+            //window.alert("Can't delete a profile while editing!");
+        }
+    }
+
+    $scope.editParameters = function() {
+        if ($scope.editingParam != true) {
+
+            $scope.revertingProfile = $.extend(true, {}, $scope.selectedProfile);
+            $scope.profile = $scope.selectedProfile;
+
+        } else {
+            //window.alert("Please save before editing a new profile");
+        }
+        $scope.editingParam = true;
+    }
+
+    $scope.activateProfile = function() {
+        $scope.activeProfile = $scope.selectedProfile.name;
+    }
+
+    $scope.saveProfile = function() {
+        $scope.editingParam = false;
+        $scope.editName = false;
+    }
+
+    $scope.revertProfile = function() {
+        $scope.profile = $.extend(true, {}, $scope.revertingProfile);
+    }
+
+    $scope.hoverIn = function() {
+        this.hoverEdit = true;
+    };
+
+    $scope.hoverOut = function() {
+        this.hoverEdit = false;
+    };
+
+    $scope.addParameter = function() {
+		if(this.newParamName != undefined){
+			$scope.profile.parameters.push(0);
+			$scope.profile.parameterNames.push(this.newParamName);
+			this.newParamName = undefined;
 		}
-	}
 
-	$scope.editParameters = function(){
-		if($scope.editingParam != true){
+    }
 
-			$scope.revertingProfile = $.extend(true, {},$scope.selectedProfile);
-			$scope.profile = $scope.selectedProfile;
-
-		}else{
-			//window.alert("Please save before editing a new profile");
-		}
-		$scope.editingParam = true;
-	}
-
-	$scope.activateProfile = function(){
-		$scope.activeProfile = $scope.selectedProfile.name;
-	}
-
-	$scope.saveProfile = function(){
-		$scope.editingParam = false;
-		$scope.editName = false;
-	}
-
-	$scope.revertProfile = function(){
-		$scope.profile = $.extend(true, {},$scope.revertingProfile);
-	}
-
-	function saveProfiles(){
-	}
+	$scope.removeParameter = function() {
+        $scope.profile.parameters.splice(this.$index,1);
+		$scope.profile.parameterNames.splice(this.$index,1);
+    }
 
 });
