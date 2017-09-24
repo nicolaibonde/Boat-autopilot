96a90e37c596947bf037aa40d224d70ec7b2a67b
Nicolai Bonde <201404519@post.au.dk>
2017-09-24 11:11:14 +0200

    Fixed an navBar edge case and fixed a bug with the navbar not indicating the tab

--- website/js/main.js
+++ website/js/main.js
@@ -1,25 +1,5 @@
 var app = angular.module("myApp", ["ngRoute", "leaflet-directive"]);
-app.config(function($routeProvider) {
-    $routeProvider
-        .when("/", {
-            templateUrl: "home.htm",
-        })
-        .when("/p2p", {
-            templateUrl: "p2p.htm",
-        })
-        .when("/coverage", {
-            templateUrl: "coverage.htm"
-        })
-        .when("/status", {
-            templateUrl: "status.htm"
-        })
-        .when("/editparameters", {
-            templateUrl: "editparameters.htm"
-        })
-        .otherwise({
-            template: "<h1><i class='glyphicon glyphicon-bullhorn'></i> Ups?</h1><p>I can't find that page, sorry</p><p>Maybe call support for me? <i class='glyphicon glyphicon-earphone'></i></p>"
-        });
-});
+
 
 
 app.factory('dataHolder', function() {
@@ -30,7 +10,3 @@ app.factory('dataHolder', function() {
 app.config(['$qProvider', function($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
 }]);
-
-app.controller('navCtrl', function($scope, $location) {
-    $scope.selectedTab = $location.path();
-});
--- website/js/nav.js
+++ website/js/nav.js
@@ -1,3 +1,33 @@
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "home.htm",
+        })
+        .when("/p2p", {
+            templateUrl: "p2p.htm",
+        })
+        .when("/coverage", {
+            templateUrl: "coverage.htm"
+        })
+        .when("/status", {
+            templateUrl: "status.htm"
+        })
+        .when("/editparameters", {
+            templateUrl: "editparameters.htm"
+        })
+        .otherwise({
+            template: "<h1><i class='glyphicon glyphicon-bullhorn'></i> Ups?</h1><p>I can't find that page, sorry</p><p>Maybe call support for me? <i class='glyphicon glyphicon-earphone'></i></p>"
+        });
+});
+
+app.controller('navCtrl', function($scope, $location) {
+    $scope.selectedTab = $location.path();
+		if($location.path() == ""){
+			$scope.selectedTab = "/";
+		}
+});
+
+
 $(document).ready(function() {
 	navbarResize();
 	$('#sidebarCollapse').on('click', function() {
