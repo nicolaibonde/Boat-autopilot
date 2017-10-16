1c5170174759dcddda2bd13c88eb7c15bf807fae
neobonde <201404519@post.au.dk>
2017-10-16 09:51:50 +0200

    added Coordinate data class

---
+++ controller/CAPTAIN/CAPTAIN/Coordinate.cpp
@@ -0,0 +1,11 @@
+#include "Coordinate.h"
+
+Coordinate::Coordinate(double latitude, double longitude)
+{
+	Latitude_ = latitude;
+	Longitude_ = longitude;
+}
+
+Coordinate::~Coordinate()
+{
+}
---
+++ controller/CAPTAIN/CAPTAIN/Coordinate.h
@@ -0,0 +1,12 @@
+#pragma once
+class Coordinate
+{
+public:
+	Coordinate(double latitude, double longitude);
+	~Coordinate();
+
+public:
+	double Latitude_;
+	double Longitude_;
+};
+
