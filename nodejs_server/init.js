1a0422bbd053b43c434461a8377c926aa63448e9
neobonde <201404519@post.au.dk>
2017-10-18 09:50:59 +0200

    updated nodejs init to work with non local init

--- nodejs_server/init.js
+++ nodejs_server/init.js
@@ -8,6 +8,10 @@ var express = require('express'),
 	path = require("path");
 
 var fs = require('fs');
+var ip = require('ip');
+
+var website_path = __dirname + '/../website'
+
 //support parsing of application/json type post data
 app.use(bodyParser.json());
 
@@ -16,15 +20,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //tell express that www is the root of our public web folder
-app.use(express.static("../website"));
-
+app.use(express.static("website_path"))
 //tell express what to do when the /form route is requested
 app.post('/profiles',function(req, res){
 	console.log(req.body);
 	res.set("Connection", "close");
 	res.setHeader('Content-Type', 'application/json');
 
-	fs.writeFile("../website/savedData/profiles.json", JSON.stringify(req.body), function(err) {
+	fs.writeFile(website_path+'/savedData/profiles.json', JSON.stringify(req.body), function(err) {
 	    if(err) {
 			res.sendStatus(500);
 	        return console.log(err);
@@ -39,7 +42,7 @@ app.post('/active',function(req, res){
 	res.set("Connection", "close");
 	res.setHeader('Content-Type', 'application/json');
 
-	fs.writeFile("../website/savedData/activeParam.json", JSON.stringify(req.body), function(err) {
+	fs.writeFile(website_path+'/savedData/activeParam.json', JSON.stringify(req.body), function(err) {
 	    if(err) {
 			res.sendStatus(500);
 	        return console.log(err);
@@ -54,7 +57,7 @@ app.post('/toNav',function(req, res){
 	res.set("Connection", "close");
 	res.setHeader('Content-Type', 'application/json');
 
-	fs.writeFile("../website/savedData/toNav.json", JSON.stringify(req.body), function(err) {
+	fs.writeFile(website_path+'/savedData/toNav.json', JSON.stringify(req.body), function(err) {
 	    if(err) {
 			res.sendStatus(500);
 	        return console.log(err);
@@ -65,14 +68,6 @@ app.post('/toNav',function(req, res){
 });
 
 //wait for a connection
-app.listen(80, function () {
-  console.log('Server is running. Point your browser to: http://localhost:80');
+app.listen(3000, function () {
+  console.log('Server is running. Point your browser to: '+ip.address()+':3000');
 });
-
-
-/*var connect = require('connect');
-var serveStatic = require('serve-static');
-
-connect().use(serveStatic('../website/')).listen(8080, function(){
-    console.log('Server running on 8080...');
-});*/
