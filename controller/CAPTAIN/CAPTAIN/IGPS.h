62667268dbc708490a6cbfb3a58ed1878ecc7407
neobonde <201404519@post.au.dk>
2017-10-18 14:42:51 +0200

    Added IGPS interface

---
+++ controller/CAPTAIN/CAPTAIN/IGPS.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Pose.h"
+#include "GPSStatus.h"
+
+class IGPS
+{
+public:
+	virtual ~IGPS(){};
+	virtual void Setup() = 0;
+	virtual Pose GetPose() = 0;
+	virtual GPSStatus GetStatus() = 0;
+};
+
