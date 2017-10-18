f6e2b89e2ad529cd4c0e6273db226385596a4f58
neobonde <201404519@post.au.dk>
2017-10-18 14:26:58 +0200

    Added IAutopilot interface

---
+++ controller/CAPTAIN/CAPTAIN/IAutopilot.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+class IAutopilot
+{
+public:
+	virtual ~IAutopilot() {};
+	virtual void Run(std::string xte_telegram) = 0;
+	virtual void Stop() = 0;
+	virtual void SetParameters(double p, double i, double d) = 0;
+};
+
