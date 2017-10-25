68d5f01644b8ce938fa93f0d172d69630f989df0
RespiceFinem <troelsringboel@gmail.com>
2017-10-25 13:48:18 +0200

    NavigationData changed to include a completed path and path vector

--- controller/CAPTAIN/CAPTAIN/NavigationData.h
+++ controller/CAPTAIN/CAPTAIN/NavigationData.h
@@ -5,11 +5,14 @@
 class NavigationData
 {
 public:
-	NavigationData(const std::vector<Coordinate> line); 
+	NavigationData(std::vector<Coordinate> completed_path, 
+		std::vector<Coordinate> polyline, int ete, double progress);
 	~NavigationData();
 
 public:
-	std::vector<Coordinate> Line_; //Consider if vector is the best list for this
+	std::vector<Coordinate> Completed_path_;
+	std::vector<Coordinate> Polyline_; //Consider if vector is the best list for this
 	int Ete_;
+	double Progress_;
 };
 
