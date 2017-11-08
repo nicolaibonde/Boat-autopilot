57936c98614b3adddac20dd88a0448db3dbbf260
neobonde <201404519@post.au.dk>
2017-11-08 12:41:52 +0100

    replaced wiringPi with pigpio, which allows for 2 pwm signals with a variable duty cycle and frequency at the same time

---
+++ controller/CAPTAIN/CAPTAIN/IGPIO.h
@@ -0,0 +1,13 @@
+#pragma once
+#pragma once
+
+class IGPIO
+{
+public:
+	virtual ~IGPIO() {};
+	virtual int GpioSetMode(unsigned gpio, unsigned mode) = 0;
+	virtual int GpioPWM(unsigned user_gpio, unsigned dutycycle) = 0;
+	virtual int GpioSetPWMfrequency(unsigned user_gpio, unsigned frequency) = 0;
+	virtual int GpioServo(unsigned user_gpio, unsigned pulsewidth) = 0;
+};
+
---
+++ controller/CAPTAIN/CAPTAIN/PiGpio.cpp
@@ -0,0 +1,31 @@
+#include "PiGpio.h"
+
+PiGpio::PiGpio()
+{
+	gpioInitialise();
+}
+
+PiGpio::~PiGpio()
+{
+	gpioTerminate();
+}
+
+int PiGpio::GpioSetMode(unsigned gpio, unsigned mode)
+{
+	return gpioSetMode(gpio, mode);
+}
+
+int PiGpio::GpioPWM(unsigned user_gpio, unsigned dutycycle)
+{
+	gpioPWM(user_gpio, dutycycle);
+}
+
+int PiGpio::GpioSetPWMfrequency(unsigned user_gpio, unsigned frequency)
+{
+	gpioSetPWMfrequency(user_gpio, frequency)
+}
+
+int PiGpio::GpioServo(unsigned user_gpio, unsigned pulsewidth)
+{
+	gpioServo(user_gpio, pulsewidth);
+}
