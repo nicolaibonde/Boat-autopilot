8d88b724b6be2e87a34647aed5a967826c7c7a5c
neobonde <201404519@post.au.dk>
2017-11-09 09:54:35 +0100

    Split test into multiple files

---
+++ controller/CAPTAIN/unit.test.CAPTAIN/DCMotor_tests.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_DYN_LINK
+#ifdef STAND_ALONE
+#   define BOOST_TEST_MODULE Main
+#endif
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(DCMotor_tests)
+BOOST_AUTO_TEST_CASE(DCMotor_output_test)
+{
+
+}
+
+BOOST_AUTO_TEST_CASE(DCMotor_output2_test)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
