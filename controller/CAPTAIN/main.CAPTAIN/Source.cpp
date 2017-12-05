37f9ad4b17504aef9aef94afea080d5d810b17a6
RespiceFinem <troelsringboel@gmail.com>
2017-12-05 08:35:01 +0100

    New main

--- controller/CAPTAIN/main.CAPTAIN/Source.cpp
+++ controller/CAPTAIN/main.CAPTAIN/Source.cpp
@@ -1,31 +1,43 @@
+#include <thread>
+#include <chrono>
+#include "CAPTAIN/SimpleSerial.h"
 #include "CAPTAIN/PiGpio.h"
+#include "CAPTAIN/Ublox_neo7m.h"
 #include "CAPTAIN/Servo.h"
 #include "CAPTAIN/DCMotor.h"
-#include <chrono>
-#include <thread>
+#include "CAPTAIN/Autopilot.h"
+#include "CAPTAIN/JSONTransmitter.h"
+#include "CAPTAIN/Navigation.h"
+#include "CAPTAIN/JSONReceiver.h"
 
-
-int main(int argc, char* argv[])
+int main()
 {
+	const std::string port = "/dev/ttyS0";
+	const unsigned int baud_rate = 9600;
+	SimpleSerial serial(port, baud_rate);
+	Ublox_neo7m gps = Ublox_neo7m(serial);
+	std::thread gps_thread = gps.Setup();
+
+	gps_thread.detach();
+
 	PiGpio servoGPIO = PiGpio();
 	Servo rudder = Servo(servoGPIO);
-
 	PiGpio motorGPIO = PiGpio();
 	DCMotor thruster = DCMotor(motorGPIO);
+	Autopilot auto_pilot = Autopilot(rudder, thruster);
 
-	float i = 0.0;
-	
-	while (i < 48.0)
+	boost::filesystem::path filepath = boost::filesystem::current_path().parent_path().parent_path().parent_path().append(
+		"website\\savedData\\");
+	JSONTransmitter transmitter = JSONTransmitter(rudder, thruster, gps, filepath.string());
+	Navigation nav = Navigation(gps, transmitter, auto_pilot);
+	JSONReceiver receiver = JSONReceiver(nav, auto_pilot, filepath.string());
+
+	while (true)
 	{
-		if (i > 50.0)
-		{
-			i = 0;
-		}
-
-		rudder.SetPosition(i);
-		thruster.SetSpeed(i);
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		i += 0.1;
+		receiver.ReceiveActiveParam();
+		receiver.ReceiveToNav();
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	}
+
+	gps.StopThread();
 }
